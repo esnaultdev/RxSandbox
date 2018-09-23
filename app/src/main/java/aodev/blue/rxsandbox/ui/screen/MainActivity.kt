68ef74088445d2be7b2d8b9220a5dd01e555acde
Ao <aodevblue@gmail.com>
2018-09-23 23:36:30 +0200

    Add an empty details fragment with nav component

---
+++ app/src/main/java/aodev/blue/rxsandbox/ui/screen/MainActivity.kt
@@ -0,0 +1,13 @@
+package aodev.blue.rxsandbox.ui.screen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import aodev.blue.rxsandbox.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
